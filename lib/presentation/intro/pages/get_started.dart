8e2143e86c00ad8b7184478bf7d3e49721b96459
Matthew McDevitt <mmcdevitt1997@gmail.com>
2024-11-04 07:28:13 -0600

    fixed the state issue

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -14,58 +15,61 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AppImages.introBG,
+                    )))),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
             padding: const EdgeInsets.symmetric(
               vertical: 40,
               horizontal: 40
             ),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                  AppImages.introBG, 
-                 )
-               )
-            ),
             child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: SvgPicture.asset(
-                  AppVectors.logo
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo),
                   ),
-                ),
-                const Spacer(),
-                const Text(
-                'Enjoy Listening To Music',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 18
-                ),
-                ),
-                const SizedBox(height: 21,),
-                const Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.grey,
-                  fontSize: 13
+                  const Spacer(),
+                  const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18),
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20,),
-                BasicAppButton(
-                  onPressed: (){
-
-                  }, 
-                  title: 'Get Started')
-              ],
-            ),
+                  const SizedBox(
+                    height: 21,
+                  ),
+                  const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.grey,
+                        fontSize: 13),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  BasicAppButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                          builder: (BuildContext context) => const ChooseModePage())
+                        );
+                      },
+                    title: 'Get Started')
+                ],
+              ),
           ),
-
-          Container(
-            color: Colors.black.withOpacity(0.15),
-          )
         ],
       ),
     );
