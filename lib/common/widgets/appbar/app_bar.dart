94b9fb89194c7b8062a36445d99fa2670067b3f8
Matthew McDevitt <mmcdevitt1997@gmail.com>
2024-11-05 08:01:32 -0600

    signup complete

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/common/helpers/is_dark_mode.dart';
 
-class BasicAppbar extends StatelessWidget {
-  const BasicAppbar({super.key});
+class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget ? title;
+  final Widget ? action;
+  final Color ? backgroundColor;
+  final bool hideBack;
+  const BasicAppbar({
+    this.title,
+    this.hideBack = false,
+    this.action,
+    this.backgroundColor ,
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.transparent,
+      backgroundColor: backgroundColor ?? Colors.transparent,
       elevation: 0,
-      leading: IconButton(
-        onPressed: () {
+      centerTitle: true,
+      title: title ?? const Text(''),
+      actions: [
+        action ?? Container()
+      ],
+      leading: hideBack ? null : IconButton(
+        onPressed: (){
           Navigator.pop(context);
         },
         icon: Container(
           height: 50,
           width: 50,
           decoration: BoxDecoration(
-              color: context.isDarkMode
-                  ? Colors.white.withOpacity(0.03)
-                  : Colors.black.withOpacity(0.04),
-              shape: BoxShape.circle),
+            color: context.isDarkMode ? Colors.white.withOpacity(0.03) : Colors.black.withOpacity(0.04),
+            shape: BoxShape.circle
+          ),
           child: Icon(
             Icons.arrow_back_ios_new,
             size: 15,
@@ -30,4 +44,7 @@ class BasicAppbar extends StatelessWidget {
       ),
     );
   }
+  
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
